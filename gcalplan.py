9e1fe94d6903aedaad262a98439a0b76aa8a906c
hdbookie <hdbooks15@gmail.com>
2023-05-17 22:20:03 -0300

    adjusted time

--- gcalplan.py
+++ gcalplan.py
@@ -20,20 +20,37 @@ calendar_service = build('calendar', 'v3', credentials=credentials)
 # Replace with your calendar ID, usually it's your email address
 calendar_id = os.environ.get('CALENDAR_ID')
 
-# Your schedule
-schedule = [
-    ('Wake up, breathing with prayer', '06:00:00', 15),
-    ('Go for jog on beach', '06:15:00', 45),
-    # Add more items here
-]
+# Access the schedule from the environment variable
+schedule_str = os.environ.get('SCHEDULE')
+
+
+# Evaluate the schedule string to convert it into a list of tuples
+schedule = eval(schedule_str)
+
+# Adjust the schedule by 15 minutes
+adjusted_schedule = []
+
+for title, start_time, duration in schedule:
+    # Convert the start time to a datetime object
+    start = datetime.datetime.strptime(
+        f'{event_date} {start_time}', '%Y-%m-%d %H:%M:%S')
+
+    # Add 15 minutes to the start time
+    adjusted_start = start + datetime.timedelta(minutes=15)
+
+    # Calculate the end time based on the adjusted start time and duration
+    end = adjusted_start + datetime.timedelta(minutes=duration)
+
+    adjusted_schedule.append(
+        (title, adjusted_start.strftime('%H:%M:%S'), duration))
 
 # Set the date for the events
-event_date = '2023-05-02'  # Change this to the desired date
+event_date = '2023-05-17'  # Change this to the desired date
 
 # Timezone
 tz = pytz.timezone('America/Sao_Paulo')
 
-for title, start_time, duration in schedule:
+for title, start_time, duration in adjusted_schedule:
     start = datetime.datetime.strptime(
         f'{event_date} {start_time}', '%Y-%m-%d %H:%M:%S')
     start = tz.localize(start)
